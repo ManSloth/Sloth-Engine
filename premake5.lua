f2c8eedff1c9fdcda880357bbd5a098f4664c150
ManSloth <lawsonj07@yahoo.com>
2022-01-21 04:31:08 -0500

    Premake
    Included premake 5 into the project

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "Sloth"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Sloth"
+	location "Sloth"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.19041.0"
+
+		defines
+		{
+			"SLTH_PLATFORM_WINDOWS",
+			"SLTH_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+	
+		filter "configurations:Debug"
+			defines "SLTH_DEBUG"
+			symbols "On"
+
+		filter "configurations:Release"
+			defines "SLTH_Release"
+			optimize "On"
+
+		filter "configurations:Dist"
+			defines "SLTH_DIST"
+			optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Sloth/vendor/spdlog/include", 
+		"Sloth/src"
+	}
+
+	links
+	{
+		"Sloth"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.19041.0"
+
+		defines
+		{
+			"SLTH_PLATFORM_WINDOWS"
+		}
+	
+		filter "configurations:Debug"
+			defines "SLTH_DEBUG"
+			symbols "On"
+
+		filter "configurations:Release"
+			defines "SLTH_Release"
+			optimize "On"
+
+		filter "configurations:Dist"
+			defines "SLTH_DIST"
+			optimize "On"
