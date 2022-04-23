0053551feaba72528e07e954b220531123de155b
ManSloth <lawsonj07@yahoo.com>
2022-04-23 03:44:41 -0400

    Windows Events
    Meshed event system with windows events that are logged. Plus the close button works!

--- Sloth/src/Sloth/Application.h
+++ Sloth/src/Sloth/Application.h
@@ -2,6 +2,8 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Sloth/Events/ApplicationEvent.h"
+
 #include "Window.h"
 
 namespace Sloth {
@@ -13,7 +15,11 @@ namespace Sloth {
 		virtual ~Application();
 
 		void Run();
+
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
