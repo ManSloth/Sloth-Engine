2cbbd141cf6fd9aafa2bd975f3eaf820721ae0f0
ManSloth <lawsonj07@yahoo.com>
2022-05-26 04:05:42 -0400

    First Triangle
    Rendered first triangle

--- Sloth/src/Platform/OpenGL/OpenGLContext.cpp
+++ Sloth/src/Platform/OpenGL/OpenGLContext.cpp
@@ -17,6 +17,11 @@ namespace Sloth
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		SLTH_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		SLTH_CORE_INFO("OpenGL Info:");
+		SLTH_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
+		SLTH_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
+		SLTH_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
