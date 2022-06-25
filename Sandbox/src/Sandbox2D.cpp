16581765f079744141dd6416ccdd10507c988d9d
ManSloth <lawsonj07@yahoo.com>
2022-06-25 06:37:47 -0400

    2D Renderer Transforms
    Added transforms (position & scale) to the 2D renderer

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -4,7 +4,6 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include "Platform/OpenGL/OpenGLShader.h"
 
 Sandbox2D::Sandbox2D()
 	:Layer("Sandbox2D"), m_CameraController(1280.0f / 720.0f)
@@ -35,10 +34,9 @@ void Sandbox2D::OnUpdate(Sloth::Timestep ts)
 	Sloth::RenderCommand::Clear();
 
 	Sloth::Renderer2D::BeginScene(m_CameraController.GetCamera());
-	Sloth::Renderer2D::DrawQuad({ 0.0f, 0.0f }, { 1.0f, 1.0f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+	Sloth::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+	Sloth::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
 	Sloth::Renderer2D::EndScene();
-	//std::dynamic_pointer_cast<Sloth::OpenGLShader>(m_FlatColorShader)->Bind();
-	//std::dynamic_pointer_cast<Sloth::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat4("u_Color", m_SquareColor);
 }
 
 void Sandbox2D::OnImGuiRender()
