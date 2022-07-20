39e4564ea18c6c26ec2a7918ea41c05a57d83657
ManSloth <lawsonj07@yahoo.com>
2022-07-20 00:14:14 -0400

    Added textures to sprite component
    Drag and drop sprites to texture slot of sprite component

--- Sloth-Editor/src/Panels/SceneHierarchyPanel.cpp
+++ Sloth-Editor/src/Panels/SceneHierarchyPanel.cpp
@@ -8,6 +8,8 @@
 #include "Sloth/Scene/Components.h"
 #include <cstring>
 
+#include <filesystem>
+
 /* The Microsoft C++ compiler is non-compliant with the C++ standard and needs
  * the following definition to disable a security warning on std::strncpy().
  */
@@ -16,6 +18,8 @@
 #endif
 
 namespace Sloth {
+
+	extern const std::filesystem::path g_AssetPath;
 
 	SceneHierarchyPanel::SceneHierarchyPanel(const Ref<Scene>& context)
 	{
@@ -328,6 +332,22 @@ namespace Sloth {
 		DrawComponent<SpriteRendererComponent>("Sprite Renderer", entity, [](auto& component)
 			{
 				ImGui::ColorEdit4("Color", glm::value_ptr(component.Color));
+
+				ImGui::Button("Texture", ImVec2(100.0f, 0.0f));
+				if (ImGui::BeginDragDropTarget())
+				{
+					if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("CONTENT_BROWSER_ITEM"))
+					{
+						const wchar_t* path = (const wchar_t*)payload->Data;
+						std::filesystem::path texturePath = std::filesystem::path(g_AssetPath) / path;
+						component.Texture = Texture2D::Create(texturePath.string());
+					}
+					ImGui::EndDragDropTarget();
+				}
+
+
+
+				ImGui::DragFloat("Tiling Factor", &component.TilingFactor, 0.1f, 0.0f, 100.0f);
 			});
 
 	}
--- Sloth/src/Sloth/Scene/Components.h
+++ Sloth/src/Sloth/Scene/Components.h
@@ -8,6 +8,7 @@
 
 #include "SceneCamera.h"
 #include "ScriptableEntity.h"
+#include "Sloth/Renderer/Texture.h"
 
 namespace Sloth {
 
@@ -45,6 +46,9 @@ namespace Sloth {
 	struct SpriteRendererComponent
 	{
 		glm::vec4 Color{ 1.0f, 1.0f, 1.0f, 1.0f };
+
+		Ref<Texture2D> Texture;
+		float TilingFactor = 1.0f;
 
 		SpriteRendererComponent() = default;
 		SpriteRendererComponent(const SpriteRendererComponent&) = default;
