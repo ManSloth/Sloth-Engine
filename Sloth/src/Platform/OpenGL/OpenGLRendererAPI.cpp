a0bdfa45338a6b11be7df3f7f17efef572a0aa27
ManSloth <lawsonj07@yahoo.com>
2022-07-27 03:50:06 -0400

    Line and Rectangles
    Added ability to draw lines and rectangles to the renderer

--- Sloth/src/Platform/OpenGL/OpenGLRendererAPI.cpp
+++ Sloth/src/Platform/OpenGL/OpenGLRendererAPI.cpp
@@ -33,6 +33,7 @@ namespace Sloth {
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 		//glEnable(GL_DEPTH_TEST);
+		glEnable(GL_LINE_SMOOTH);
 	}
 
 #ifdef HZ_DEBUG
@@ -63,5 +64,16 @@ namespace Sloth {
 		vertexArray->Bind();
 		uint32_t count = indexCount ? indexCount : vertexArray->GetIndexBuffer()->GetCount();
 		glDrawElements(GL_TRIANGLES, count, GL_UNSIGNED_INT, nullptr);
+	}
+
+	void OpenGLRendererAPI::DrawLines(const Ref<VertexArray>& vertexArray, uint32_t vertexCount)
+	{
+		vertexArray->Bind();
+		glDrawArrays(GL_LINES, 0, vertexCount);
+	}
+
+	void OpenGLRendererAPI::SetLineWidth(float width)
+	{
+		glLineWidth(width);
 	}
 }
--- Sloth/src/Sloth/Scene/Scene.cpp
+++ Sloth/src/Sloth/Scene/Scene.cpp
@@ -227,6 +227,7 @@ namespace Sloth {
 					auto [transform, sprite] = group.get<TransformComponent, SpriteRendererComponent>(entity);
 
 					Renderer2D::DrawSprite(transform.GetTransform(), sprite, (int)entity);
+					Renderer2D::DrawRect(transform.GetTransform(), glm::vec4(1.0f), (int)entity);
 				}
 			}
 
@@ -258,6 +259,7 @@ namespace Sloth {
 				auto [transform, sprite] = group.get<TransformComponent, SpriteRendererComponent>(entity);
 
 				Renderer2D::DrawSprite(transform.GetTransform(), sprite, (int)entity);
+				Renderer2D::DrawRect(transform.GetTransform(), glm::vec4(1.0f), (int)entity);
 			}
 		}
 
